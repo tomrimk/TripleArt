c69b98a571cf546d23eccb1d03f9e9f54ced11f0
Tomas Rimkus <rimk.tom@gmail.com>
2017-12-17 00:31:21 +0200

    index conf

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,11 @@ module.exports = (app, passport) => {
   // handle the callback after facebook has authenticated the user
   app.get(
     '/auth/facebook/callback',
-    passport.authenticate('facebook', {
-      successRedirect: '/profile'
-    })
+    passport.authenticate('facebook', { failureRedirect: '/' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    }
   );
 
   // =====================================
