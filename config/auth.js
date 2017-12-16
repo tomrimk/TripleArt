b1a422403047e6609d56e07861ed20efa9fc9e6d
Tomas Rimkus <rimk.tom@gmail.com>
2017-12-17 00:12:18 +0200

    Changed callback url

--- config/auth.js
+++ config/auth.js
@@ -3,5 +3,5 @@ var keys = require('../keys/prod');
 module.exports = {
   clientID: keys.facebookAppId, // your App ID
   clientSecret: keys.facebookAppSecret, // your App Secret
-  callbackURL: 'http://localhost:8080/auth/facebook/callback'
+  callbackURL: '/auth/facebook/callback'
 };
