8a3a2fdd224de7b729fd962d1c8a77db8c0eb8f7
Tomas Rimkus <rimk.tom@gmail.com>
2017-12-17 00:06:44 +0200

    more small changes

--- keys/prod.js
+++ keys/prod.js
@@ -1,6 +1,5 @@
 module.exports = {
-  facebookAppId = process.env.FACEBOOK_APP_ID,
-  facebookAppSecret = process.env.FACEBOOK_APP_SECRET,
-  mongoDbUri = process.env.MONGODB_URI
+  facebookAppId: process.env.FACEBOOK_APP_ID,
+  facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
+  mongoDbUri: process.env.MONGODB_URI
 };
-
